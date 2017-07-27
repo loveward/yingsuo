823d6a7815bedd4dc6f183301ffadc3b90f28a18
loveward <loveward94@gmail.com>
2017-07-27 17:49:34 +0800

    设置获取到的时间为系统时间，方便调用运行，并加入了权限提示

--- shadowsocks/crypto/getChinatime.py
+++ shadowsocks/crypto/getChinatime.py
@@ -1,15 +1,21 @@
 #-*-coding:utf8 -*-
 import httplib as client
 import time
+import os
+
 def get_webservertime(host):
     conn=client.HTTPConnection(host)
     conn.request("GET", "/")
     r=conn.getresponse()
     ts=  r.getheader('date')
-    local_time= time.mktime(time.strptime(ts[5:], "%d %b %Y %H:%M:%S GMT")) + (8 * 60 * 60)
+    local_time= time.mktime(time.strptime(ts[5:], "%d %b %Y %H:%M:%S GMT")) + (16 * 60 * 60)
     ltime = time.gmtime(local_time)
     dat = 'date -u -s "%d-%d-%d %d:%d:%d" ' % (ltime.tm_year,ltime.tm_mon,ltime.tm_mday,ltime.tm_hour,ltime.tm_min,ltime.tm_sec)
-    print dat
+    #print dat
+    #print local_time
+    a= os.system(dat)
+    if a == 256:
+      print "设置时间失败，请用更高权限运行，系统时间必须为互联网时间才可以进行加密计算。"
 
 get_webservertime('www.baidu.com')
 
